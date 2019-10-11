ee7b5da276634b755e974d82837bb5bf21b69f26
Noam <noams@epd-medical.com>
2019-10-11 20:36:20 +0300

    moved all validators to folder

---
+++ Assets/Scripts/Sandbox/Test.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Sandbox
+{
+    public class Test : MonoBehaviour
+    {
+        void Start()
+        {
+            var start = new Vector3(1, 2, 3);
+            var end = new Vector3(0, 0, 0);
+            Debug.DrawLine(start, end, Color.blue, 200);
+        }
+    }
+}
