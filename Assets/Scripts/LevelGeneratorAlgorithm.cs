ac7507f8d8308ac143d627936fe0a7eee8f435fb
Sagie <sagielevy21@gmail.com>
2019-10-11 21:14:05 +0300

    Fixed infinite loop

--- Assets/Scripts/LevelGeneratorAlgorithm.cs
+++ Assets/Scripts/LevelGeneratorAlgorithm.cs
@@ -6,37 +6,32 @@ namespace Assets.Scripts
 {
     public class LevelGeneratorAlgorithm : ILevelGenerator
     {
-        LevelConfiguration ILevelGenerator.GenerateLevel(int minItemCount, int maxItemCount, float worldRadius, float minRotationSpeed, float maxRotationSpeed, float minScale, float maxScale)
+        LevelConfiguration ILevelGenerator.GenerateLevel(int minItemCount, int maxItemCount,
+            float worldRadius, float minRotationSpeed, float maxRotationSpeed, float minScale,
+            float maxScale)
         {
-
-            int objsAmount = Random.Range(minItemCount, maxItemCount);
+            int objsAmount = Random.Range(minItemCount, maxItemCount + 1);
             Item[] levelObjects = new Item[objsAmount];
+
             for (int i = 0; i <= objsAmount; i++)
             {
                 Item obj = new Item();
                 obj.id = i;
-                obj.parentId = this.PickParent(i, i);
+                obj.parentId = Random.Range(0, i);
                 obj.scale = Random.Range(minScale, maxScale);
                 obj.rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
-                obj.startingPosition = new Vector3(Random.Range(0, worldRadius), Random.Range(0, worldRadius), Random.Range(0, worldRadius)) ;
-                obj.rotateDirection = new Vector3(Random.Range(0, worldRadius), Random.Range(0, worldRadius), Random.Range(0, worldRadius));
-                levelObjects[i] = obj;    
+                obj.startingPosition = new Vector3(Random.Range(0, worldRadius),
+                    Random.Range(0, worldRadius), Random.Range(0, worldRadius));
+                obj.rotateDirection = new Vector3(Random.Range(0, worldRadius),
+                    Random.Range(0, worldRadius), Random.Range(0, worldRadius));
+                levelObjects[i] = obj;
             }
+
             Vector3 solutionPosition = new Vector3();
             LevelConfiguration levelConfig = new LevelConfiguration();
             levelConfig.items = levelObjects;
             levelConfig.solutionStartPosition = solutionPosition;
             return levelConfig;
-        }
-
-        private int PickParent(int son_id, int objsNum)
-        {
-            int j = 0;
-            while (j == son_id)
-            {
-                j = Random.Range(0, objsNum);
-            }
-            return j;
         }
     }
 }
