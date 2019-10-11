d688a22e1ed287f19f5d8a9a067b051c02e7230b
Sagie <sagielevy21@gmail.com>
2019-10-11 14:24:55 +0300

    Added basic script

---
+++ Assets/Scripts/RotateItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class RotateItem : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 1.5f;
+    [SerializeField] private float moveSpeed = 0.5f;
+    [SerializeField] private Vector3 rotationVector = Vector3.zero;
+    [SerializeField] private Vector3 directionVector = Vector3.zero;
+
+    void Start()
+    {
+        directionVector = directionVector.normalized;
+        rotationVector = rotationVector.normalized;
+    }
+
+    void Update()
+    {
+        transform.position += directionVector * Time.deltaTime * moveSpeed;
+        transform.rotation *= Quaternion.Euler(rotationVector * Time.deltaTime * rotateSpeed);
+    }
+}
