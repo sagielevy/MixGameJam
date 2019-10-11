d4beb960dafe899117cb5d887f0522f24ed13d2b
Sagie <sagielevy21@gmail.com>
2019-10-11 23:33:49 +0300

    Start solution generator

--- Assets/Scripts/SolutionTrailGenerator.cs
+++ Assets/Scripts/SolutionTrailGenerator.cs
@@ -6,21 +6,51 @@ namespace Assets.Scripts
 {
     public class SolutionTrailGenerator : MonoBehaviour, ITrail
     {
+        [SerializeField] private FloatReference sampleIntervalSeconds;
+        [SerializeField] private FloatReference animateTimeSeconds;
         [SerializeField] private FloatReference speedFactor;
         [SerializeField] private float simulationSpeed;
 
         private List<Vector3> samples;
         private float startTime;
+        private float lastRealInterval;
+        private float realInterval;
+        private float realAnimateTime;
 
         private void Start()
         {
             samples = new List<Vector3>();
             speedFactor.SetValue(simulationSpeed);
+            realInterval = sampleIntervalSeconds.GetValue() / speedFactor.GetValue();
+            realAnimateTime = animateTimeSeconds.GetValue() / speedFactor.GetValue();
+            startTime = Time.time;
         }
 
         public List<Vector3> GetSampledLocations()
         {
             return samples;
+        }
+
+        private void FixedUpdate()
+        {
+            if (Time.time - startTime >= realAnimateTime)
+            {
+                StopSimulation();
+                return;
+            }
+
+            if (Time.time - lastRealInterval >= realInterval)
+            {
+                lastRealInterval = Time.time;
+                samples.Add(transform.position);
+            }
+        }
+
+        private void StopSimulation()
+        {
+            speedFactor.SetValue(1);
+
+            // TODO call generate level again. Send generated samples
         }
     }
 }
--- Assets/Scripts/TrailSpawner.cs
+++ Assets/Scripts/TrailSpawner.cs
@@ -11,14 +11,16 @@ namespace Assets.Scripts
         [SerializeField] private FloatReference animateTimeSeconds;
 
         private TrailRenderer trailRenderer;
-        private float lastSampleTime;
         private List<Vector3> samples;
+        private float startTime;
+        private float lastSampleTime;
 
         private void Start()
         {
             samples = new List<Vector3>();
             trailRenderer = GetComponent<TrailRenderer>();
-            trailRenderer.time = animateTimeSeconds.GetValue();
+            trailRenderer.time = animateTimeSeconds.GetValue(); // TODO set more?
+            startTime = Time.time;
         }
 
         public List<Vector3> GetSampledLocations()
@@ -28,6 +30,11 @@ namespace Assets.Scripts
 
         private void FixedUpdate()
         {
+            if (Time.time - startTime >= animateTimeSeconds.GetValue())
+            {
+                return;
+            }
+
             if (Time.time - lastSampleTime >= sampleIntervalTimeSeconds.GetValue())
             {
                 lastSampleTime = Time.time;
