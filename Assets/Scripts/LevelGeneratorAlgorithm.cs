2987e5ef35126cfa96bb1e8da0c3fa4eeb7b2ed5
matans <matans@cs.huji.ac.il>
2019-10-11 17:41:30 +0300

    implemented ILevelGenerator interface, compiles but before tests.

--- Assets/Scripts/LevelGeneratorAlgorithm.cs
+++ Assets/Scripts/LevelGeneratorAlgorithm.cs
@@ -1,17 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using UnityEngine;
+
 
 namespace Assets.Scripts
 {
     public class LevelGeneratorAlgorithm : ILevelGenerator
     {
-        public ItemData[] ItemPositions(int minItemCount, int maxItemCount, float worldRadius,
-            float minRotationSpeed, float maxRotationSpeed, float minScale, float maxScale)
+        LevelConfiguration ILevelGenerator.GenerateLevel(int minItemCount, int maxItemCount, float worldRadius, float minRotationSpeed, float maxRotationSpeed, float minScale, float maxScale)
         {
-            throw new NotImplementedException();
+
+            int objsAmount = Random.Range(minItemCount, maxItemCount);
+            Item[] levelObjects = new Item[objsAmount];
+            for (int i = 0; i <= objsAmount; i++)
+            {
+                Item obj = new Item();
+                obj.id = i;
+                obj.parentId = this.pickParent(i, objsAmount);
+                obj.scale = Random.Range(minScale, maxScale);
+                obj.rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+                obj.startingPosition = new Vector3(Random.Range(0, worldRadius), Random.Range(0, worldRadius), Random.Range(0, worldRadius)) ;
+                obj.rotateDirection = new Vector3(Random.Range(0, worldRadius), Random.Range(0, worldRadius), Random.Range(0, worldRadius));
+                levelObjects[i] = obj;    
+            }
+            Vector3 solutionPosition = new Vector3();
+            LevelConfiguration levelConfig = new LevelConfiguration();
+            levelConfig.items = levelObjects;
+            levelConfig.solutionStartPosition = solutionPosition;
+            return levelConfig;
+        }
+
+        private int pickParent(int son_id, int objsNum)
+        {
+            int j = 0;
+            while (j == son_id)
+            {
+                j = Random.Range(0, objsNum);
+            }
+            return j;
         }
     }
 }
