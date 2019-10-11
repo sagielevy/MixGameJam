97d49cd69225d70406130ee939b2c3efcb512a3b
Noam <noams@epd-medical.com>
2019-10-11 17:50:50 +0300

    Added ITrailValidator

--- Assets/Scripts/ITrail.cs
+++ Assets/Scripts/ITrail.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
-    public interface ITrailValidator
+    public interface ITrail
     {
-        bool Validate(ITrail expected, ITrail actual);
+        List<Vector3> getSampledLocations();
     }
 }
