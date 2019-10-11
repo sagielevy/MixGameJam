4d9fb6c3b104872a247fe080bfa65851be11f038
Guy <guy.kornblit@mail.huji.ac.il>
2019-10-11 18:04:44 +0300

    movement create file- camera

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed;
+    public Transform target;
+
+    void Update()
+    {
+        transform.LookAt(target);
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            transform.Translate(transform.up * Input.GetAxis("Vertical") * Time.deltaTime * speed); //.up = positive y
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Translate(transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed); //.right = positive x
+        }
+    }
+}
