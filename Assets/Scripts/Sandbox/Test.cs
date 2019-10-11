0fd728ffa0e45f00981dde1b6810de34d67e39a5
Noam <noams@epd-medical.com>
2019-10-11 22:35:26 +0300

    added line generator for testing

--- Assets/Scripts/Sandbox/Test.cs
+++ Assets/Scripts/Sandbox/Test.cs
@@ -1,14 +1,25 @@
 ﻿using UnityEngine;
+using Assets.Scripts.TrailValidators.MockData;
+using System;
+using System.Linq;
+using UnityEngine;
 
 namespace Assets.Scripts.Sandbox
 {
     public class Test : MonoBehaviour
     {
+        [SerializeField] private int numberOfLines;
         void Start()
         {
-            var start = new Vector3(1, 2, 3);
-            var end = new Vector3(0, 0, 0);
-            Debug.DrawLine(start, end, Color.blue, 200);
+            Vector3 prev = new Vector3(0,0,0);
+
+            foreach (int index in Enumerable.Range(0, numberOfLines))
+            {
+                var dataFactory = new MockDataFactory();
+                var current = dataFactory.YieldNewPoint();
+                Debug.DrawLine(prev, current, Color.blue, 200);
+                prev = current;
+            }
         }
 
         void Update()
