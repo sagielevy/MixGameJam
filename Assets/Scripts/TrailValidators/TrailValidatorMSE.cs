3b14fd0a3a5007e82867afb5bcd361a543bb7810
Noam <noams@epd-medical.com>
2019-10-13 01:40:56 +0300

    added fix for sampling shift

--- Assets/Scripts/TrailValidators/TrailValidatorMSE.cs
+++ Assets/Scripts/TrailValidators/TrailValidatorMSE.cs
@@ -16,13 +16,21 @@ namespace Assets.Scripts
             var expectedSamples = expected.GetSampledLocations();
             var actualSamples = actual.GetSampledLocations();
 
-            var diff = expectedSamples.Zip(actualSamples, (e, a) => e-a);
-            var normDiff = diff.Select(d => d.magnitude);
-            var sumNormDiff = normDiff.Aggregate(0.0, (x, y) => x + y);
-            var mse = sumNormDiff / diff.Count();
-            bool isValid = mse <= threshold;
-            Debug.Log($"mse: {mse}");
-            return isValid;
+            int count = expectedSamples.Count();
+            for (int i = 0; i < count / 2; i++)
+            {
+                var diff = expectedSamples.Skip(i).Zip(actualSamples.Take(count-i), (e, a) => e - a);
+                var normDiff = diff.Select(d => d.magnitude);
+                var sumNormDiff = normDiff.Aggregate(0.0, (x, y) => x + y);
+                var mse = sumNormDiff / diff.Count();
+                bool isValid = mse <= threshold;
+                Debug.Log($"mse: {mse}");
+                if (isValid)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
