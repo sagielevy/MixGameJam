d34ac1002c90e88359f3ce1fdb48d2b1fb90a98f
Noam <noams@epd-medical.com>
2019-10-12 11:37:18 +0300

    added drawing of "moving" data, which is shifted by its own phase

--- Assets/Scripts/Sandbox/Test.cs
+++ Assets/Scripts/Sandbox/Test.cs
@@ -15,28 +15,49 @@ namespace Assets.Scripts.Sandbox
         [SerializeField] private float y0;
         [SerializeField] private float at;
         [SerializeField] private float bt;
-        [SerializeField] private float c;
+        [SerializeField] private float z0;
         [SerializeField] private float dx;
         [SerializeField] private float ey;
         [SerializeField] private float fxy;
         [SerializeField] private float gxx;
         [SerializeField] private float hyy;
 
+        [SerializeField] private float acosx;
+        [SerializeField] private float bsiny;
+        [SerializeField] private float hz;
+        [SerializeField] private float tphaseReference;
+        [SerializeField] private float tphaseMoving;
+
+        [SerializeField] private float samplingRate;
+
         void Start()
         {
-            Vector3 prev = new Vector3(0,0,0);
-            var dataFactory = new MockDataFactory(this);
-            foreach (int index in Enumerable.Range(0, numberOfLines))
-            {
-                var current = dataFactory.YieldNewPoint();
-                Debug.LogFormat($"current: {current}, prev: {prev}");
-                Debug.DrawLine(prev, current, Color.blue, 200);
-                prev = current;
-            }
         }
 
         void Update()
         {
+            void DrawLine(Vector3 start, Vector3 end, Color color)
+            {
+                Debug.LogFormat($"start: {start}, end: {end}");
+                Debug.DrawLine(start, end, color, 0.1f);
+            }
+            void DrawLinesByPointFactory(MockDataFactory factory, Color color)
+            {
+                Vector3 prev = new Vector3(0, 0, 0);
+                foreach (int index in Enumerable.Range(0, numberOfLines))
+                {
+                    var current = factory.YieldNewPoint();
+                    DrawLine(prev, current, color);
+                    prev = current;
+                }
+            }
+            
+            var referenceDataFactory = new MockDataFactory(this, tphaseReference);
+            var movingDataFactory = new MockDataFactory(this, tphaseMoving);
+
+            DrawLinesByPointFactory(referenceDataFactory, Color.blue);
+            DrawLinesByPointFactory(movingDataFactory, Color.red);
+
 
         }
 
@@ -48,14 +69,15 @@ namespace Assets.Scripts.Sandbox
             private IEnumerator<Vector3> dataPointsGenerator;
             private Test test;
 
-            public MockDataFactory(Test te)
+            public MockDataFactory(Test te, float phase)
             {
                 test = te;
                 t = 0;
-                dataPointsGenerator = DataPointsGenerator();
+                //dataPointsGenerator = parabolaPointsGenerator();
+                dataPointsGenerator = SpiralPointsGenerator(phase);
             }
 
-            public IEnumerator<Vector3> DataPointsGenerator()
+            public IEnumerator<Vector3> parabolaPointsGenerator()
             {
                 while (true)
                 {
@@ -63,7 +85,22 @@ namespace Assets.Scripts.Sandbox
                     // z(t) = c + dx + ey + fxy + gxx + hyy
                     var x = test.x0 + test.at * t;
                     var y = test.y0 + test.bt * t;
-                    var z = test.c + test.dx * x + test.ey * y + test.fxy * x * y + test.gxx * x * x + test.hyy * y * y;
+                    var z = test.z0 + test.dx * x + test.ey * y + test.fxy * x * y + test.gxx * x * x + test.hyy * y * y;
+                    yield return new Vector3(x, y, z);
+                    t++;
+                }
+            }
+
+            public IEnumerator<Vector3> SpiralPointsGenerator(float phase)
+            {
+                while (true)
+                {
+                    // x = a cos(bt)+x0
+                    // y = b sin(dt)+y0
+                    // z = gt+z0
+                    var x = test.x0 + test.acosx * Mathf.Cos(test.samplingRate * Mathf.PI * t + phase);
+                    var y = test.y0 + test.bsiny * Mathf.Sin(test.samplingRate * Mathf.PI * t + phase);
+                    var z = test.z0 + test.hz * t;
                     yield return new Vector3(x, y, z);
                     t++;
                 }
