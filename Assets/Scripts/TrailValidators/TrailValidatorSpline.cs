9044adf9b2365516c2da025720331543cb290cf5
Noam <noams@epd-medical.com>
2019-12-16 22:51:25 +0200

    added numsharp. started using it

---
+++ Assets/Scripts/TrailValidators/TrailValidatorSpline.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Numpy;
+using Python.Runtime;
+
+namespace Assets.Scripts
+{
+    class TrailValidatorSpline : ITrailValidator
+    {
+        private float threshold = Mathf.Infinity;
+        private int spline;
+
+        public bool Validate(ITrail expected, ITrail actual, float threshold)
+        {
+            var expectedSamples = expected.GetSampledLocations();
+            var actualSamples = actual.GetSampledLocations();
+
+            var (np_exp, np_act) = prepare_np_input(expectedSamples, actualSamples);
+
+            // we know that the curves are sampled at same time intervals. If we didn't we would need to interpolate
+            // both of them (cubic spline or other) and sample at equal times or referably at equal distances.
+            // Since we do know, we can skip this phase.
+            // Resampling at same-spacial-distances could help, but it would be negligible here.
+
+            //normalize both curves to zero mean to be able to find curves that look the same but are far away
+            var one = new int[1]; one[0] = 1;
+           
+            var exp_mean = np.mean(np_exp, one);
+            var exp_normalized = np_exp - exp_mean;
+            var act_mean = np.mean(np_act, one);
+            var act_normalized = np_act - act_mean;
+
+
+
+            return true;
+        }
+
+        private static (Numpy.NDarray<double>, Numpy.NDarray<double>) prepare_np_input(IEnumerable<Vector3> expectedSamples, IEnumerable<Vector3> actualSamples)
+        {
+            double[,] dexp = new double[expectedSamples.Count(), 3];
+            double[,] dact = new double[actualSamples.Count(), 3];
+            for (var i = 0; i < expectedSamples.Count(); ++i)
+            {
+                for (var j = 0; j < 3; ++j)
+                {
+                    dexp[i, j] = expectedSamples.ElementAt(i)[j];
+                    dact[i, j] = actualSamples.ElementAt(i)[j];
+                }
+            }
+            var np_exp = np.array(dexp);
+            var np_act = np.array(dact);
+
+            return (np_exp, np_act);
+        }
+    }
+}
